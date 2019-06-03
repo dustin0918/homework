9604fa662693eb17f64f12128c8b7bbed0692403
Steve Godin <steve.god@gmail.com>
2019-06-03 09:38:12 -0700

    Write removeFromList function

--- todo-list/solution.js
+++ todo-list/solution.js
@@ -3,3 +3,8 @@ const list = []
 function addToList(thing, list) {
   list.push(thing)
 }
+
+function removeFromList(index, list) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+  list.splice(index, 1)
+}
